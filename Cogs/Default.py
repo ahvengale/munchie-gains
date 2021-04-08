0740e84884d1d246f8a06cea2dba135490727b8e
Quinn Hirt <hirtquinn@gmail.com>
2021-04-08 01:50:56 -0400

    basic cog functionality and bot starting

---
+++ Cogs/Default.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from discord.ext import commands
+
+
+def setup(bot):
+    bot.add_cog(Default(bot))
+
+class Default(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        latency = round(self.bot.latency * 1000)
+        await ctx.send(f'{latency} ms')
