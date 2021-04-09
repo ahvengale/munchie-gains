00314c5db18c3372254f245cfee19c18ddf1ae63
Quinn Hirt <hirtquinn@gmail.com>
2021-04-09 14:49:20 -0400

    workout logging implemented, cleaned up code

--- Cogs/AdminCommands.py
+++ Cogs/AdminCommands.py
@@ -11,19 +11,25 @@ from discord.ext import commands
 cluster = MongoClient('mongodb://localhost:27017')
 db = cluster['munchie-gains']
 
+val_pattern = '[a-zA-Z ]*[0-9]*x[0-9]* @ [0-9]*:.*'
+
+patterns = ['^[a-zA-Z ]*', '(?<= )[0-9]*(?=x)', '(?<=x)[0-9]*(?= )', '(?<=@ )[0-9]*(?=:)', '(?<=: ).*$']
+pat_keys = ['workout', 'sets', 'reps', 'weight', 'comments']
+
+log_temp = {}
+
 def setup(bot):
     bot.add_cog(AdminCommands(bot))
 
 def isUserEnrolled(user):
     collection = db['members']
     results = collection.find({'discord_id':user})
-    for x in results:
-        if results:
-            return True
-        else:
-            return False
+    if results:
+        return True
+    else:
+        return False
 
-def regex_parse(patterns, text):
+def regex_parse(text):
     ret = []
     for p in patterns:
         ret.append(re.findall(p, text)[0].strip())
@@ -58,13 +64,24 @@ class AdminCommands(commands.Cog):
             await ctx.send('You were not enrolled anyways.')
 
     @commands.command()
-    async def log(self, ctx, *, text):
-        val_pattern = '[a-zA-Z ]*[0-9]*x[0-9]* @ [0-9]*:.*'
-        if re.findall(val_pattern, text):
-            workout = '^[a-zA-Z ]*'
-            sets = '(?<= )[0-9]*(?=x)'
-            reps = '(?<=x)[0-9]*(?= )'
-            weight = '(?<=@ )[0-9]*(?=:)'
-            comment = '(?<=: ).*$'
-            print(regex_parse([workout, sets, reps, weight, comment], text))
+    async def log(self, ctx, *, text=''):
+        if re.findall(val_pattern, text) and str(ctx.message.author.id) in log_temp.keys():
+            post = dict(zip(pat_keys, regex_parse(text)))
+            log_temp[str(ctx.message.author.id)]['workouts'].append(post)
+        else:
+            await ctx.send('You did not begin logging a workout.')
+
+    @commands.command()
+    async def start(self, ctx, *, text='unspecified'):
+        if str(ctx.message.author.id) in log_temp.keys() and isUserEnrolled(ctx.message.author.id):
+            await ctx.send('You are already logging a workout.')
+        else:
+            log_temp[str(ctx.message.author.id)] = {'discord_id': ctx.message.author.id, 'workout_day_type': text, 'workouts': []}
+
+    @commands.command()
+    async def stop(self, ctx):
+        if str(ctx.message.author.id) in log_temp.keys() and isUserEnrolled(ctx.message.author.id):
+            print(log_temp.pop(str(ctx.message.author.id)))
+        else:
+            await ctx.send('You were not logging a workout.')
 
